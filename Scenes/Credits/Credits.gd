65bcc8c69cda7061d256c9f7f634bef89af02508
Danny Ralde <danny.ralde@theappmaster.com>
2025-10-01 11:54:52 -0300

    renombra archivos

--- Scenes/Credits/Credits.gd
+++ Scenes/Credits/Credits.gd
@@ -5,9 +5,5 @@ extends Control
 func _ready():
 	music_credits.play()
 
-func _on_quit_pressed():
-	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
-
-
 func _on_texture_button_pressed() -> void:
 	get_tree().change_scene_to_file(GLOBALS.SCENE.MENU)
